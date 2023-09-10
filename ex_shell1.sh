4ae1363a564762ea377a7672d10a22f02e0bb9a3
Subho Ghosh <subho.3600.ghosh@gmail.com>
2023-09-10 18:31:29 +0530

    script demo1

---
+++ ex_shell1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+####################################################
+# Author: Subho Ghosh
+# date: 10/09/23
+#
+# This is script outputs the node health
+#
+# Version: v1
+####################################################
+
+df -h
+
+free -g
+
+nproc
